0a4eb70a3bd0794c6bd4b17533f5fd22984a8c72
shrmpy <>
2022-07-02 19:55:19 -0700

    workflow

--- README.md
+++ README.md
@@ -1,2 +1,70 @@
 # fgj2022
-entry for My First Game Jam: summer 2022
+For myFirst Game Jam entry
+
+Summer 2022 Heal theme of
+ [My First Game Jam](https://itch.io/jam/my-first-game-jam-summer-2022)
+
+When done 🤞, download will be on the
+ [itch.io site here](https://shrmpy.itch.io/fgj2022)
+
+
+## Quickstart
+```bash
+git clone https://github.com/shrmpy/fgj2022
+cd fgj2022 && go build 
+./fgj2022
+```
+## Build in Local Container
+```bash
+cd fgj2022
+docker build -t bc .
+docker run -ti --rm --entrypoint sh -v $PWD:/opt/test bc
+go build -o test
+cp test /opt/test/fgj2022
+exit
+./fgj2022
+```
+## Make your own snap package
+[![fgj2022](https://snapcraft.io/fgj2022/badge.svg)](https://snapcraft.io/fgj2022)
+```bash
+# ub server includes a empty lxd?
+sudo snap remove --purge lxd
+# reinstall lxd
+sudo snap install lxd
+sudo lxd init --auto
+sudo usermod -a -G lxd ${USER}
+# view config
+lxc version
+lxc profile show default
+lxc storage show default
+echo 'export SNAPCRAFT_BUILD_ENVIRONMENT=lxd' >> ~/.profile
+sudo reboot
+# retrieve YAML 
+git clone https://github.com/shrmpy/fgj2022.git
+cd fgj2022
+# make snap 
+snapcraft
+# local install
+sudo snap install fgj2022_0.0.1_arm64.snap --dangerous
+# start program
+fgj2022
+```
+
+
+## Credits
+
+Github workflow
+ by [Siôn le Roux](https://github.com/sinisterstuf/ebiten-game-template) ([LICENSE](https://github.com/sinisterstuf/ebiten-game-template/blob/main/LICENSE))
+
+Font Renderer
+ by [tinne26](https://github.com/tinne26/etxt)
+ ([LICENSE](https://github.com/tinne26/etxt/blob/main/LICENSE))
+
+Ebitengine
+ by [Hajime Hoshi](https://github.com/hajimehoshi/ebiten/)
+ ([LICENSE](https://github.com/hajimehoshi/ebiten/blob/main/LICENSE))
+
+DejaVu Sans Mono
+ by [DejaVu](https://dejavu-fonts.github.io/)
+ ([LICENSE](https://github.com/dejavu-fonts/dejavu-fonts/blob/master/LICENSE))
+
---
+++ main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	_ "embed"
+
+	"fmt"
+	"image/color"
+	"log"
+)
+import (
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
+	"github.com/tinne26/etxt"
+)
+
+//go:generate cp $GOROOT/misc/wasm/wasm_exec.js dist/web/wasm_exec.js
+//go:generate env GOOS=js GOARCH=wasm go build -ldflags "-w -s" -o dist/web/fgj2022.wasm ./
+//go:embed DejaVuSansMono.ttf
+var dejavuSansMonoTTF []byte
+
+
+func init() {
+	log.SetFlags(log.Lshortfile | log.Ltime)
+}
+func main() {
+	var (
+		err    error
+		name   string
+		wd, ht = 640, 480
+		ch     = make(chan string, 100)
+		fonts  = etxt.NewFontLibrary()
+	)
+	defer close(ch)
+	if name, err = fonts.ParseFontBytes(dejavuSansMonoTTF); err != nil {
+		log.Fatalf("FAIL Parse DejaVu Sans Mono, %s", err.Error())
+	}
+	log.Printf("INFO font, %s", name)
+	var renderer = etxt.NewStdRenderer()
+	renderer.SetCacheHandler(etxt.NewDefaultCache(2 * 1024 * 1024).NewHandler())
+	renderer.SetFont(fonts.GetFont("DejaVu Sans Mono"))
+	renderer.SetColor(color.White)
+	renderer.SetSizePx(12)
+	ebiten.SetWindowSize(wd, ht)
+	ebiten.SetWindowTitle("fgj2022")
+	var game = &Game{
+		Width:   wd,
+		Height:  ht,
+		txtre:   renderer,
+		history: make([]string, 0, 25),
+	}
+
+
+
+	if err = ebiten.RunGame(game); err != nil {
+		log.Fatalf("FAIL main, %s", err.Error())
+	}
+}
+
+// Update runs game logic steps
+func (g *Game) Update() error {
+	// Pressing Q any time quits immediately
+	if ebiten.IsKeyPressed(ebiten.KeyQ) {
+		return fmt.Errorf("INFO Quit key")
+	}
+
+	// Pressing F toggles full-screen
+	if inpututil.IsKeyJustPressed(ebiten.KeyF) {
+		var fs = ebiten.IsFullscreen()
+		ebiten.SetFullscreen(!fs)
+	}
+
+
+	// TODO 
+
+
+
+
+	return nil
+}
+
+// Draw renders one frame
+func (g *Game) Draw(screen *ebiten.Image) {
+	g.printHistory(screen)
+
+
+}
+
+func (g *Game) printHistory(screen *ebiten.Image) {
+	// help us troubleshoot
+	g.txtre.SetTarget(screen)
+	max := len(g.history)
+
+	g.txtre.SetAlign(etxt.Bottom, etxt.Left)
+	for i := max; i > 0; i-- {
+		msg := g.history[i-1]
+		sz := g.txtre.SelectionRect(msg)
+		g.txtre.Draw(msg, 0, g.Height-sz.Height.Ceil()*i)
+	}
+	// print frame rate in se corner
+	g.txtre.SetAlign(etxt.Bottom, etxt.Right)
+	g.txtre.Draw(fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()), g.Width-1, g.Height)
+}
+
+
+// Game represents the main game state
+type Game struct {
+	Width   int
+	Height  int
+
+	txtre   *etxt.Renderer
+	history []string
+
+}
+
+
+// Layout is static for now, can be dynamic
+func (g *Game) Layout(outsideWidth int, outsideHeight int) (int, int) {
+	return g.Width, g.Height
+}
+
+// allow maze to bubble-up debug msg
+func (g *Game) AddHistory(tmp string, values ...any) {
+	var msg = fmt.Sprintf(tmp, values...)
+	if len(g.history) < 25 {
+		g.history = append(g.history, msg)
+	}
+}
+
+
