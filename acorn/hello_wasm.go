52e176cce01f967b03df6b7c06ecce84d86c44d2
shrmpy <>
2022-07-03 17:07:11 -0700

    skeleton

--- acorn/hello_wasm.go
+++ acorn/hello_wasm.go
@@ -2,7 +2,6 @@
 package acorn
 
 import (
-	_ "embed"
 
 	//"log"
 )
@@ -11,16 +10,11 @@ type Acorn struct {
 	info func(string, ...any)
 }
 func NewAcorn(fn func(string, ...any)) *Acorn {
-	var err error
 	ac := Acorn{ info: fn }
 	return &ac
 }
 func (a *Acorn) Update() error {
 
-	/*
-	if ebiten.IsKeyPressed(ebiten.KeySpace) {
-		flite.TextToSpeech("Hello World", g.voice, "play")
-	}*/
 
 	return nil
 }
