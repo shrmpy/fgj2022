49dd7623decaa4c0ba06fd6dc033b9be55addb3e
shrmpy <>
2022-07-09 02:22:31 -0700

    wazero experimental

--- flite.go
+++ flite.go
@@ -4,61 +4,55 @@ package main
 
 import (
 	"context"
-	"embed"
 	_ "embed"
 	"log"
-	"io/fs"
+	//"io/fs"
 	"os"
 )
 import (
 	"github.com/tetratelabs/wazero"
-	"github.com/tetratelabs/wazero/sys"
+	"github.com/tetratelabs/wazero/experimental"
 	"github.com/tetratelabs/wazero/wasi_snapshot_preview1"
 )
-//go:embed dist/web
-var flite_fs embed.FS
+
+
 //go:embed dist/testout.wav
 var flite_WAV []byte
 //go:embed dist/flite.wasm
 var flite_wasm []byte
 
+var tmpFS = os.DirFS(".")
 func fliteTest(speak string) {
 	var ctx = context.Background()
 	var rt = wazero.NewRuntimeWithConfig(wazero.NewRuntimeConfig().WithWasmCore2())
 	defer rt.Close(ctx)
-	tmp, err := fs.Sub(flite_fs, "dist")
-	if err != nil {
-		log.Panicf("FAIL fs, %s", err.Error())
-	}
 	var cfg = wazero.NewModuleConfig().
 		WithStdout(os.Stdout).
-		WithStderr(os.Stderr).
-		WithFS(tmp)
+		WithStderr(os.Stderr)
+
 
 	if _, err := wasi_snapshot_preview1.Instantiate(ctx, rt); err != nil {
 		log.Panicf("FAIL wazero, %s", err.Error())
 	}
+	ctx, closer := experimental.WithFS(ctx, tmpFS)
+	defer closer.Close(ctx)
+
 	code, err := rt.CompileModule(ctx, flite_wasm, wazero.NewCompileConfig())
 	if err != nil {
 		log.Panicf("FAIL compile, %s", err.Error())
 	}
 
+	cfg.WithArgs("wasi", speak, "output.wav")
+	_, err = rt.InstantiateModule(ctx, code, cfg)
+	if err != nil {
+		log.Panicf("FAIL wasi, %s", err.Error())
+	}
+/*
 	// InstantiateModule runs the "_start" function, WASI's "main".
 	// * Set the program name (arg[0]) to "wasi"; arg[1] should be "/test.txt".
-	cfg.WithArgs("wasi", speak, "output.wav")
-	if _, err = rt.InstantiateModule(ctx, code, cfg); err != nil {
-
-		// Note: Most compilers do not exit the module after running "_start",
-		// unless there was an error. This allows you to call exported functions.
-		if exitErr, ok := err.(*sys.ExitError); ok && exitErr.ExitCode() != 0 {
-			log.Printf("INFO exit_code: %d", exitErr.ExitCode())
-		} else if !ok {
-			log.Panicf("FAIL wasi, %s", err.Error())
-		}
-	}
-
-/*	fli, err := rt.InstantiateModule(ctx, flite_wasm, cfg)
-	results, err := xfn.Call(ctx, args, "output.wav")
-	*/
+	//cfg.WithArgs("wasi", speak, "output.wav")
+	xfn := code.ExportedFunction("_start")
+	_, err := xfn.Call(ctx, "wasi", speak, "output.wav")
+*/
 }
 
